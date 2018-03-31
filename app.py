931e8f4b890561dcbb09b535e531f5c60e74e43a
Davide Masserut <d.masserut@gmail.com>
2018-03-31 14:01:15 +0200

    Handle 404 status code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, abort, jsonify, render_template
 from models import Drivers, Events, Reports, Vehicles
 from playhouse.shortcuts import model_to_dict
 
@@ -9,37 +9,49 @@ app.config.from_pyfile('dev_config.py', silent=True)
 @app.route('/api/drivers/')
 @app.route('/api/drivers/<int:id>')
 def get_drivers(id=None):
-    if id is None:
-        query = Drivers.select().order_by(Drivers.id)
-        return jsonify([model_to_dict(row) for row in query])
-    return jsonify(model_to_dict(Drivers.get_by_id(id)))
+    try:
+        if id is None:
+            query = Drivers.select().order_by(Drivers.id)
+            return jsonify([model_to_dict(row) for row in query])
+        return jsonify(model_to_dict(Drivers.get_by_id(id)))
+    except Drivers.DoesNotExist:
+        abort(404)
 
 
 @app.route('/api/events/')
 @app.route('/api/events/<int:id>')
 def get_events(id=None):
-    if id is None:
-        query = Events.select().order_by(Events.id)
-        return jsonify([model_to_dict(row) for row in query])
-    return jsonify(model_to_dict(Events.get_by_id(id)))
+    try:
+        if id is None:
+            query = Events.select().order_by(Events.id)
+            return jsonify([model_to_dict(row) for row in query])
+        return jsonify(model_to_dict(Events.get_by_id(id)))
+    except Events.DoesNotExist:
+        abort(404)
 
 
 @app.route('/api/reports/')
 @app.route('/api/reports/<int:id>')
 def get_reports(id=None):
-    if id is None:
-        query = Reports.select().order_by(Reports.id)
-        return jsonify([model_to_dict(row) for row in query])
-    return jsonify(model_to_dict(Reports.get_by_id(id)))
+    try:
+        if id is None:
+            query = Reports.select().order_by(Reports.id)
+            return jsonify([model_to_dict(row) for row in query])
+        return jsonify(model_to_dict(Reports.get_by_id(id)))
+    except Reports.DoesNotExist:
+        abort(404)
 
 
 @app.route('/api/vehicles/')
 @app.route('/api/vehicles/<int:id>')
 def get_vehicles(id=None):
-    if id is None:
-        query = Vehicles.select().order_by(Vehicles.id)
-        return jsonify([model_to_dict(row) for row in query])
-    return jsonify(model_to_dict(Vehicles.get_by_id(id)))
+    try:
+        if id is None:
+            query = Vehicles.select().order_by(Vehicles.id)
+            return jsonify([model_to_dict(row) for row in query])
+        return jsonify(model_to_dict(Vehicles.get_by_id(id)))
+    except Vehicles.DoesNotExist:
+        abort(404)
 
 
 @app.route('/')
@@ -48,5 +60,4 @@ def index():
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
     app.run()
